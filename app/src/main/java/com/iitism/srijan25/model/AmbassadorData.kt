a8eb58e4bbec154a46a2912f74580abb1547b2f4
SKS <sks0202005@gmail.com>
2025-01-04 14:25:16 +0530

    Warnings fixed

--- app/src/main/java/com/iitism/srijan25/model/AmbassadorData.kt
+++ app/src/main/java/com/iitism/srijan25/model/AmbassadorData.kt
@@ -1,7 +1,7 @@
 package com.iitism.srijan25.model
 
-data class AmbassadorData(
-    val refCode:String,
-    val referrals:Int,
-    val revenue:Int
-)
+//data class AmbassadorData(
+//    val refCode:String,
+//    val referrals:Int,
+//    val revenue:Int
+//)
--- app/src/main/java/com/iitism/srijan25/model/LeaderboardEntry.kt
+++ app/src/main/java/com/iitism/srijan25/model/LeaderboardEntry.kt
@@ -1,8 +1,8 @@
 package com.iitism.srijan25.model
 
-data class LeaderboardEntry(
-    val ambassador:String,
-    val referrals: Int,
-    val revenue: Int,
-    val id: String?
-)
+//data class LeaderboardEntry(
+//    val ambassador:String,
+//    val referrals: Int,
+//    val revenue: Int,
+//    val id: String?
+//)
